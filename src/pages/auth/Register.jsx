96138affe8ae7553224980b93e696bb4fb07441c
Animeshhit <animeshkumbhakarhit2@gmail.com>
2024-05-21 01:50:56 +0530

    register page responsive

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -126,17 +126,19 @@ const Register = ({ setLoadingProgress }) => {
     <>
       <section className="register__screen mt-16">
         <div className="container mx-auto px-4 sm:px-0 h-full">
-          <div className="flex items-center h-full">
-            <div className="left__container w-1/2 flex items-center justify-center h-[500px]">
+          <div className="flex flex-col sm:flex-row items-center h-full">
+            <div className="left__container w-[100px] h-[100px] sm:w-1/3 md:w-1/2 hidden sm:flex items-center justify-center sm:h-[500px]">
               <img
                 src="/Register.svg"
                 alt="register here"
                 className="w-full h-full"
               />
             </div>
-            <div className="right__container w-1/2">
-              <h1 className="font-Karla font-bold text-3xl">Register here</h1>
-              <p className="text-sm mt-3 leading-6  pr-24">
+            <div className="right__container w-full sm:w-2/3 md:w-1/2">
+              <h1 className="font-Karla font-bold text-xl sm:text-3xl">
+                Register here
+              </h1>
+              <p className="text-xs sm:text-sm mt-3 leading-6 sm:pr-24">
                 "Welcome to{" "}
                 <strong className="text-blue-600">The Craftly shop</strong>{" "}
                 We're excited to have you onboard. Start exploring and
@@ -148,7 +150,7 @@ const Register = ({ setLoadingProgress }) => {
                   Label="Mobile"
                   Type="Number"
                   PlaceHolder="e.g 8637XX84X4"
-                  InputStyles="w-3/4"
+                  InputStyles="w-full sm:w-3/4"
                   name="mobile"
                   Value={userData.mobile}
                   handleChange={handleChange}
@@ -159,7 +161,7 @@ const Register = ({ setLoadingProgress }) => {
                   Label="Password"
                   Type="password"
                   PlaceHolder="Password"
-                  InputStyles="w-3/4"
+                  InputStyles="w-full sm:w-3/4"
                   ContainerStyles="mt-4"
                   name="password"
                   Value={userData.password}
@@ -169,11 +171,11 @@ const Register = ({ setLoadingProgress }) => {
                 <button
                   type="submit"
                   disabled={registerBtn}
-                  className="w-3/4 bg-zinc-800 hover:bg-zinc-600 transition text-white py-3 px-4 text-sm rounded-md mt-8"
+                  className="w-full sm:w-3/4 bg-zinc-800 hover:bg-zinc-600 transition text-white py-3 px-4 text-sm rounded-md mt-8"
                 >
                   Register
                 </button>
-                <p className="text-sm font-Karla w-3/5 mx-auto mt-2">
+                <p className="text-sm font-Karla w-full text-center sm:text-left sm:w-3/5 mx-auto mt-2">
                   Already have an account?{" "}
                   <NavLink
                     to="/auth/login"
